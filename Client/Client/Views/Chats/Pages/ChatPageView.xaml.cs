415adb0afd70e169129b463faf2c8934ea54ece5
Dũng TT <dungtienthe1920@gmail.com>
2025-05-02 05:19:32 +0700

    Client : View-Chat- Develop silderbar and filter btn

--- Client/Client/Views/Chats/Pages/ChatPageView.xaml.cs
+++ Client/Client/Views/Chats/Pages/ChatPageView.xaml.cs
@@ -1,4 +1,8 @@
-﻿using System.Text;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -9,14 +13,14 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace Client
+namespace Client.Views.Chats.Pages
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for ChatPageView.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class ChatPageView : Page
     {
-        public MainWindow()
+        public ChatPageView()
         {
             InitializeComponent();
         }
