37dfde97e74ab465b387c2cd337a52071019a9d3
HnaKhoa222 <21522221@gm.uit.edu.vn>
2025-07-11 20:49:32 +0700

    update register

--- SocialMediaMini.Shared/SocialMediaMini.Shared/Dto/Request/Request_RegisterDTO.cs
+++ SocialMediaMini.Shared/SocialMediaMini.Shared/Dto/Request/Request_RegisterDTO.cs
@@ -10,8 +10,8 @@ namespace SocialMediaMini.Shared.Dto.Request
     public class Request_RegisterDTO
     {
         [Required(ErrorMessage = "Tên đăng nhập không được để trống!")]
-        [MinLength(6,ErrorMessage ="Tên tài khoản phải ít nhất 6 kí tự!")]
-        [MaxLength(100,ErrorMessage ="Tên tài khoản không được vượt quá 100 kí tự!")]
+        [MinLength(6,ErrorMessage = "Tên tài khoản phải ít nhất 6 kí tự!")]
+        [MaxLength(100,ErrorMessage = "Tên tài khoản không được vượt quá 100 kí tự!")]
         public string UserName { get; set; }
 
         [Required(ErrorMessage = "Mật khẩu không được để trống!")]
@@ -20,13 +20,16 @@ namespace SocialMediaMini.Shared.Dto.Request
         public string Password { get; set; }
 
         [Required(ErrorMessage = "Email không được để trống!")]
-        [EmailAddress(ErrorMessage ="Địa chỉ Email không hợp lệ!")]
-        [MaxLength(300,ErrorMessage ="Địa chỉ Email không được quá 300 kí tự!")]
+        [EmailAddress(ErrorMessage = "Địa chỉ Email không hợp lệ!")]
+        [MaxLength(300, ErrorMessage = "Địa chỉ Email không được quá 300 kí tự!")]
         public string Email { get; set; }
 
         [Required(ErrorMessage = "Số điện thoại không được để trống!")]
         [Phone(ErrorMessage = "Số điện thoại không hợp lệ!")]
         [MaxLength(30, ErrorMessage = "Số điện thoại không được quá 30 kí tự!")]
         public string PhoneNumber { get; set; }
+
+        public string EncryptionPublicKey { get; set; } // Thêm để nhận từ client
+        public string IV { get; set; } // Thêm để nhận từ client
     }
 }
